04fb7e82785b146433f91354cf6de89360a510e1
16201323 <16201323@users.noreply.github.com>
2026-09-11 14:22:58 +0800

    cleanup: remove viewport debug scripts vp1/vp2/vp3

--- vp1.py
+++
@@ -1,5 +0,0 @@
-import unreal
-ues = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem)
-ues.set_level_viewport_camera_info(unreal.Vector(8000, -8000, 5000), unreal.Rotator(pitch=-25, yaw=45, roll=0))
-if hasattr(ues, "set_level_viewport_fov"):
-    ues.set_level_viewport_fov(90.0)
